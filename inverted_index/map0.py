3e633743c3dbd38a53ad83f0719992dc49dc2165
Tyler <tylhuang@umich.edu>
2022-11-30 14:44:12 -0500

    push

--- inverted_index/map0.py
+++ inverted_index/map0.py
@@ -10,29 +10,28 @@ import sys
 # For documents with very large doc_body
 csv.field_size_limit(sys.maxsize)
 
-stop_words = []
+stop_words = set() 
 current_dir = os.path.dirname(__file__)
 with open(os.path.join(current_dir ,'stopwords.txt'), 'r') as f:
-  stop_words = f.read().splitlines()
+  for word in f.read().splitlines():
+    stop_words.add(word)
 
 
 def remove_stop_words(word):
-  if word in stop_words:
-    return False
-  return True
+  return word not in stop_words
 
 
 for line in sys.stdin:
-    document = line.split(',')
-    doc_id = int(document[0][1:-1])
+  document = line.split(',')
+  doc_id = int(document[0][1:-1])
 
-    # Join doc_title and doc_body and strip remove all non-alphanumerics
-    joined_title_body = ' '.join(document[1:])
-    text = re.sub(r"[^a-zA-Z0-9 ]+", "", joined_title_body).casefold()
+  # Join doc_title and doc_body and strip remove all non-alphanumerics
+  joined_title_body = ' '.join(document[1:])
+  text = re.sub(r"[^a-zA-Z0-9 ]+", "", joined_title_body).casefold()
 
-    # Remove all stop words
-    terms = text.split()
-    filtered_terms = list(filter(remove_stop_words, terms))
-    
-    print(doc_id)
-    print(filtered_terms)
+  # Remove all stop words
+  terms = text.split()
+  filtered_terms = list(filter(remove_stop_words, terms))
+  
+  print(doc_id)
+  print(filtered_terms)
