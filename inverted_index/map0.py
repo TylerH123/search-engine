d41f8c699a3fe2392adbd55b005ec06f3ce35523
Jeff Lin <jeff.andrew.lin@gmail.com>
2022-11-30 14:34:47 -0500

    Progress in map

--- inverted_index/map0.py
+++ inverted_index/map0.py
@@ -1,1 +1,38 @@
 """Map 0."""
+
+#!/usr/bin/env python3
+"""Word count mapper."""
+import csv
+import os
+import re
+import sys
+
+# For documents with very large doc_body
+csv.field_size_limit(sys.maxsize)
+
+stop_words = []
+current_dir = os.path.dirname(__file__)
+with open(os.path.join(current_dir ,'stopwords.txt'), 'r') as f:
+  stop_words = f.read().splitlines()
+
+
+def remove_stop_words(word):
+  if word in stop_words:
+    return False
+  return True
+
+
+for line in sys.stdin:
+    document = line.split(',')
+    doc_id = int(document[0][1:-1])
+
+    # Join doc_title and doc_body and strip remove all non-alphanumerics
+    joined_title_body = ' '.join(document[1:])
+    text = re.sub(r"[^a-zA-Z0-9 ]+", "", joined_title_body).casefold()
+
+    # Remove all stop words
+    terms = text.split()
+    filtered_terms = list(filter(remove_stop_words, terms))
+    
+    print(doc_id)
+    print(filtered_terms)
