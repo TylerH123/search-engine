4769970df591114aaa33a6f06f758864ebfe5bbb
Tyler <tylhuang@umich.edu>
2022-12-01 16:09:56 -0500

    push

--- inverted_index/reduce0.py
+++ inverted_index/reduce0.py
@@ -7,27 +7,23 @@ def reduce_one_group(key, group):
     """Reduce one group."""
     document_count = 0
     for item in group:
-      document_count = item.split()[1]
+        document_count += int(item.partition("\t")[2])
     print(f'{key} {document_count}')
-
 
 def keyfunc(line):
     """Return the key from a TAB-delimited key-value pair."""
     return line.partition("\t")[0]
-
 
 def main():
     """Divide sorted lines into groups that share a key."""
     for key, group in itertools.groupby(sys.stdin, keyfunc):
         reduce_one_group(key, group)
 
-
 if __name__ == "__main__":
     main()
 
-
-# madoop \
-#   -input example_input \
-#   -output output \
-#   -mapper map0.py \
-#   -reducer reduce0.py
+# # madoop \
+# #   -input example_input \
+# #   -output output \
+# #   -mapper map0.py \
+# #   -reducer reduce0.py
