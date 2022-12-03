eee6c08af27bc402d9397a0285fe831122810aa4
Jeff Lin <jeff.andrew.lin@gmail.com>
2022-12-03 18:16:51 -0500

    Additional

--- index_server/index/__init__.py
+++ index_server/index/__init__.py
@@ -8,7 +8,5 @@ import index.api
 app.config['INDEX_PATH'] = os.getenv('INDEX_PATH', 'inverted_index_1.txt')
 index.api.load_index()
 
-
-
 # Command to run flask app:
 # flask --app index --debug run --host 0.0.0.0 --port 9000
