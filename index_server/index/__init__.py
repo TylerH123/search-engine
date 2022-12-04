07c32a1a1975f75f8502724f7184b83b313c822f
Jeff Lin <jeff.andrew.lin@gmail.com>
2022-12-04 13:07:32 -0500

    added text cleaning for incoming queries

--- index_server/index/__init__.py
+++ index_server/index/__init__.py
@@ -10,5 +10,12 @@ pagerank = {}
 app.config['INDEX_PATH'] = os.getenv('INDEX_PATH', 'inverted_index_1.txt')
 index.api.load_index(inverted_index, pagerank)
 
+stop_words = set() 
+current_dir = os.path.dirname(__file__)
+with open(os.path.join(current_dir ,'stopwords.txt'), 'r') as f:
+  for word in f.read().splitlines():
+    stop_words.add(word)
+
+
 # Command to run flask app:
 # flask --app index --debug run --host 0.0.0.0 --port 9000
--- index_server/index/api/main.py
+++ index_server/index/api/main.py
@@ -1,88 +1,103 @@
+import collections
 import flask
 import index
 import os
-import collections
+import re
+
 
 def load_index(inverted_index, pagerank):
-  index_path = index.app.config['INDEX_PATH']
-  index_path = os.getcwd() + "/index/inverted_index/" + index_path
-  with open(index_path, "r") as file:
-    for line in file:
-      line = line.split(" ")
-      inverted_index[line[0]] = (" ".join(line[1:])).strip()
-  pagerank_path = os.getcwd() + "/index/pagerank.out"
-  with open(pagerank_path, "r") as file:
-    for line in file: 
-      line = line.strip().partition(",")
-      pagerank[int(line[0])] = float(line[2])
+    index_path = index.app.config['INDEX_PATH']
+    index_path = os.getcwd() + "/index/inverted_index/" + index_path
+    with open(index_path, "r") as file:
+        for line in file:
+            line = line.split(" ")
+            inverted_index[line[0]] = (" ".join(line[1:])).strip()
+    pagerank_path = os.getcwd() + "/index/pagerank.out"
+    with open(pagerank_path, "r") as file:
+        for line in file:
+            line = line.strip().partition(",")
+            pagerank[int(line[0])] = float(line[2])
 
 
 @index.app.route('/api/v1/')
 def get_api():
     """Get api routes."""
     context = {
-      'hits': '/api/v1/hits/',
-      'url': '/api/v1/'
+        'hits': '/api/v1/hits/',
+        'url': '/api/v1/'
     }
     return flask.jsonify(**context), 200
 
 
 @index.app.route('/api/v1/hits/')
 def get_hits():
-  """Return hits from query."""
-  query = flask.request.args.get('q')
-  weight = flask.request.args.get('w') or 0.5
-  process_query(query)
-  context = {
-    'hits': []
-  }
-  return flask.jsonify(**context), 200
+    """Return hits from query."""
+    query = flask.request.args.get('q')
+    weight = flask.request.args.get('w') or 0.5
+
+    query_terms_list = clean_query(query)
+    process_query(query_terms_list)
+    
+    context = {
+        'hits': []
+    }
+    return flask.jsonify(**context), 200
+
+
+def remove_stop_words(word):
+  return word not in index.stop_words
+
+
+def clean_query(query):
+    text = re.sub(r"[^a-zA-Z0-9 ]+", "", query).casefold()
+    terms = text.split()
+    filtered_terms = list(filter(remove_stop_words, terms))
+    return filtered_terms
 
 
 def process_query(query):
-  idf = {} 
-  query = query.split(" ") 
-  query_freq = collections.defaultdict(int)
-  term_freq = {}
-  terms = []
-  norm_fac = {}
-  for word in query: 
-    if word not in query_freq:
-      terms.append(word)
-    query_freq[word] += 1
-  result_set = get_resulting_docs(terms, idf, term_freq)
-  query_vec = get_query_vector(query_freq, idf, terms)
-  doc_vec = get_doc_vector()
+    idf = {}
+    query_freq = collections.defaultdict(int)
+    term_freq = {}
+    terms = []
+    norm_fac = {}
+    for word in query:
+        if word not in query_freq:
+            terms.append(word)
+        query_freq[word] += 1
+    result_set = get_resulting_docs(terms, idf, term_freq)
+    query_vec = get_query_vector(query_freq, idf, terms)
+    doc_vec = get_doc_vector()
 
 
 def get_resulting_docs(terms, idf, tf):
-  result_set = []
-  for word in terms: 
-    result = set()
-    docs = index.inverted_index[word]
-    docs = docs.split(" ")
-    idf[word] = float(docs[0])
-    i = 1
-    while i < len(docs): 
-      result.add(int(docs[i]))
-      i += 3
-    result_set.append(result)
-  result_docs = result_set[0]
-  for rs in result_set: 
-    result_docs = rs.intersection(result_docs)
-  return result_docs
+    result_set = []
+    for word in terms:
+        result = set()
+        docs = index.inverted_index[word]
+        docs = docs.split(" ")
+        idf[word] = float(docs[0])
+        i = 1
+        while i < len(docs):
+            result.add(int(docs[i]))
+            i += 3
+        result_set.append(result)
+    result_docs = result_set[0]
+    for rs in result_set:
+        result_docs = rs.intersection(result_docs)
+    return result_docs
 
 
-def get_query_vector(freq, idf, terms): 
-  query_vec = []
-  for word in terms: 
-    result = freq[word] * idf[word]
-    query_vec.append(result)
-  return query_vec
+def get_query_vector(freq, idf, terms):
+    query_vec = []
+    for word in terms:
+        result = freq[word] * idf[word]
+        query_vec.append(result)
+    return query_vec
 
 
 def get_doc_vector():
-  return
+    return
 
 
 # @index.app.route('/api/test/')
